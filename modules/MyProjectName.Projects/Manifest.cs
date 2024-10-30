927ee5047a10ed5b48f1f592e3a026a1c420817e
Anto Subash <antosubash@live.com>
2024-10-30 17:15:42 +0100

    Add initial project structure and configuration for MyProjectName.Cms

---
+++ modules/MyProjectName.Projects/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "MyProjectName.Projects",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "MyProjectName.Projects",
+    Category = "Content Management"
+)]
