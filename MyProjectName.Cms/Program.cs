d92e0e6d9906094fdf7ed3c1c4b4248474b7ad2e
Anto Subash <antosubash@live.com>
2024-10-31 12:28:42 +0100

    fix: Configure forwarded headers in Program.cs for better proxy support

--- MyProjectName.Cms/Program.cs
+++ MyProjectName.Cms/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.HttpOverrides;
 using OrchardCore.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,12 +13,18 @@ builder.Services
     // .Configure( (app, routes, services) => {
     // })
 ;
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders =
+        ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+    app.UseForwardedHeaders();
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
